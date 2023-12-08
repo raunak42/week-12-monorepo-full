d2c75aef36e0f7b7f6eae0440b232d2da241c7bc
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2023-12-09 01:24:39 +0530

    testing yaml mk3, made changes in ui

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,5 +1,5 @@
 "use client";
 
 export const Button = () => {
-  return <button onClick={() => alert("boop11 boop")}>Boop</button>;
+  return <button onClick={() => alert("boop11 boop test yaml")}>Boop</button>;
 };
