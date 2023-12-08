07b6bca342456a07fbcfe20b010b929cb5fc7c18
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2023-12-09 00:14:34 +0530

    oploading the repo

---
+++ packages/common/src/index.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+export const UserInput = z.object({
+    email: z.string(),
+    password: z.string()
+});
+
+export type UserInputType = z.infer<typeof UserInput> //You don't need to use zod validation on the frontend, only types are good enough. Zod is meant to be used on the backend.
+
+export type UserInputType2 = {
+    email: string,
+    password: string
+}//both types are the same.
