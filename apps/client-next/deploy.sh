501454d97e7e0e5bdf7d82ece071a93c1fa62ca6
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2023-12-09 22:45:19 +0530

    updated scripts

--- apps/client-next/deploy.sh
+++ apps/client-next/deploy.sh
@@ -2,7 +2,8 @@
 export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.1/bin
 
 cd /home/ubuntu/week-12-monorepo-full
-git pull origin main
+sudo git pull origin main
+yarn install
 yarn build
 pm2 stop next
 pm2 start npm --name "next" -- run "start:next"
