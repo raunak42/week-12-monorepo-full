1a80d59dd5c94969f5a3b5b400d5cc3adb099cbe
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2023-12-09 18:05:42 +0530

    updated root scripts to run apps on specified ports

--- apps/express-backend/src/index.ts
+++ apps/express-backend/src/index.ts
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3002
+const port = 3004
 import { UserInput } from "common" 
+
+app.use(express.json());
 
 app.get('/', (req: any, res: any) => {
   let parsedUser = UserInput.safeParse(req.body);
